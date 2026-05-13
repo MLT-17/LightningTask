98425f236851cee1430baedf200c02f347140d24
Matthias <137054661+MatTy271285@users.noreply.github.com>
2026-05-13 18:40:47 +0200

    Initial commit

---
+++ LightningTask/LightningTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightningTaskApp.swift
+//  LightningTask
+//
+//  Created by Matthias Tyca on 13.05.26.
+//
+
+import SwiftUI
+
+@main
+struct LightningTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
